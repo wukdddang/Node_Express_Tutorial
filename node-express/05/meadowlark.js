c09703bbc48bb32cf56ea3c8fe9f1a59c38d35ae
wukddang <43228743+funkyblues@users.noreply.github.com>
2023-05-30 16:58:30 +0900

    ch05 - 테스트 공부 진행중

---
+++ node-express/05/meadowlark.js
@@ -0,0 +1,37 @@
+const express = require("express");
+const expressHandlebars = require("express-handlebars");
+
+const handlers = require("./lib/handler.js");
+const fortune = require("./lib/fortune.js");
+
+const app = express();
+
+app.engine(
+  "handlebars",
+  expressHandlebars({
+    defaultLayout: "main",
+  })
+);
+app.set("view engine", "handlebars");
+
+app.use(express.static(__dirname + "/public"));
+
+const port = process.env.PORT || 3000;
+app.get("/", handlers.home);
+
+app.get("/about", handlers.about);
+
+app.use(handlers.notFound);
+
+app.use(handlers.serverError);
+
+if (require.main === module) {
+  app.listen(port, () =>
+    console.log(
+      `Express started on http://localhost:${port}; ` +
+        `press Ctrl-C to terminate.`
+    )
+  );
+} else {
+  module.exports = app;
+}
