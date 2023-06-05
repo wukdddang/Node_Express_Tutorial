9f9ca59bd175c2f1629ba0d4c38cd76c8f296f23
wukddang <43228743+funkyblues@users.noreply.github.com>
2023-06-05 22:50:39 +0900

    Ch06: 학습 완료

--- node-express/06/meadowlark.js
+++ node-express/06/meadowlark.js
@@ -1,24 +1,25 @@
-const express = require('express')
-const expressHandlebars = require('express-handlebars')
-const bodyParser = require('body-parser')
-const app = express()
+const express = require("express");
+const expressHandlebars = require("express-handlebars");
+const bodyParser = require("body-parser");
+const app = express();
 
 // the following is needed to use views
-app.engine('handlebars', expressHandlebars({ defaultLayout: 'main' }))
-app.set('view engine', 'handlebars')
+app.engine("handlebars", expressHandlebars({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
 
 // this is necessary to parse form responses
-app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
-app.get('/thank-you', (req, res) => res.render('10-thank-you'))
+app.get("/thank-you", (req, res) => res.render("10-thank-you"));
 
 // see the views/10-home.hbs file for the contents of this view
-app.get('*', (req, res) => res.render('10-home'))
+app.get("*", (req, res) => res.render("10-home"));
 
-app.post('/process-contact', (req, res) => {
-  console.log(`received contact from ${req.body.name} <${req.body.email}>`)
-  res.redirect(303, '/thank-you')
-})
+app.post("/process-contact", (req, res) => {
+  console.log(`received contact from ${req.body.name} <${req.body.email}>`);
+  res.redirect(303, "/thank-you");
+});
 
-const port = process.env.PORT || 3000
-app.listen(port, () => console.log(`\nnavigate to http://localhost:${port}\n`))
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`\nnavigate to http://localhost:${port}\n`));
